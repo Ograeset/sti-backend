e4902398d01f9c6e491f85a495efe2e17e8f0e8e
Ograeset <tedros963@gmail.com>
2022-02-11 11:28:12 +0100

    updates

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use('/healthcheck', require('./routes.healtcheck.routes'));
-appp.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(cors())
 
-appp.get("/", (req ,res)=>{
+app.get("/", (req ,res)=>{
 headers={"http_status":200, "cache-control": "no-cache"}
 body={"status": "available"}
 res.status(200).send(body)
 })
 
 app.listen(PORT , ()=>{
-  console.log('STARTED LISTENING ON PORT ${PORT}')
+  console.log(`STARTED LISTENING ON PORT ${PORT}`)
 });
